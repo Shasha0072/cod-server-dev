1a29ca88f51283671fab1f9055b1a005f2b65fe4
Shashwat Adhau <shashwat.adhau@syncious.onmicrosoft.com>
2025-05-16 14:58:36 +0530

    docker run

---
+++ dockerRun.sh
@@ -0,0 +1,16 @@
+docker run -d \
+  --name code-server \
+  -p 8443:8443 \
+  -v "/home/shashwat/.config/code-server:/home/coder/.config" \
+  -v "/home/shashwat/.config/code-server/certificates:/home/coder/certificates" \
+  -v "/home/shashwat/.local/share/code-server:/home/coder/.local/share/code-server" \
+  -v "/mnt/syncstore/shashwat:/home/coder/workspace" \
+  -u "$(id -u shashwat):$(id -g shashwat)" \
+  -e "PASSWORD=qwerty" \
+  --restart=always \
+  code-server-python \
+  --cert=/home/coder/certificates/cert.pem \
+  --cert-key=/home/coder/certificates/key.pem \
+  --bind-addr=0.0.0.0:8443 \
+  --auth=password \
+  /home/coder/workspace
